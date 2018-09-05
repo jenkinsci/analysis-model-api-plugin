c0ab5254e5560fe646f7f070a09f99a66aa3f84f
Ulli Hafner <ullrich.hafner@gmail.com>
2018-09-05 12:50:45 +0200

    Added a dummy class to make hpi-plugin and InjectedTests happy.

---
+++ src/main/java/io/jenkins/plugins/analysis/AnalysisModelApi.java
@@ -0,0 +1,11 @@
+package io.jenkins.plugins.analysis;
+
+/**
+ * Dummy class to make Jenkins HPI plug-in and InjectedTests happy.
+ *
+ * @author Ullrich Hafner
+ */
+@SuppressWarnings("unused")
+public class AnalysisModelApi {
+    // empty
+}
