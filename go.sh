c677cf495af11b0e999df77cdc47fb29fa62781b
Ulli Hafner <ullrich.hafner@gmail.com>
2018-09-08 01:00:41 +0200

    Added scripts.

---
+++ go.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -z "$JENKINS_HOME" ]]; then
+    echo "JENKINS_HOME is not defined" 1>&2
+    exit 1
+fi
+
+mvn clean install || { echo "Build failed"; exit 1; }
+
+rm -rf $JENKINS_HOME/plugins/analysis-model-api*
+
+cp -fv target/*.hpi $JENKINS_HOME/plugins
+
+cd $JENKINS_HOME
+./go.sh
+
+
