bb5dcc6e891faa232f95e8dc2616b49ea12092c7
Ulli Hafner <ullrich.hafner@gmail.com>
2019-06-30 23:55:27 +0200

    Simplify batch script.

--- go.sh
+++ go.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-if [[ -z "$JENKINS_HOME" ]]; then
-    JENKINS_HOME=../docker/volumes/jenkins-home
-    echo "JENKINS_HOME is not defined, using $JENKINS_HOME"
-fi
+JENKINS_HOME=../docker/volumes/jenkins-home
 
 mvn clean install || { echo "Build failed"; exit 1; }
 
 echo "Installing plugin in $JENKINS_HOME"
+
 rm -rf $JENKINS_HOME/plugins/analysis-model*
 cp -fv target/analysis-model-api.hpi $JENKINS_HOME/plugins/analysis-model-api.jpi
 
