c522b7c0512a111f2866152714d9e7016c1973ad
Ulli Hafner <ullrich.hafner@gmail.com>
2021-03-02 23:25:47 +0100

    Fix deploy step to docker.

--- go.sh
+++ go.sh
@@ -13,7 +13,7 @@ cp -fv target/analysis-model-api.hpi $JENKINS_HOME/plugins/analysis-model-api.jp
 
 CURRENT_UID="$(id -u):$(id -g)"
 export CURRENT_UID
-IS_RUNNING=$(docker-compose ps -q jenkins-master)
+IS_RUNNING=$(docker-compose ps -q jenkins-controller)
 if [[ "$IS_RUNNING" != "" ]]; then
     docker-compose restart
     echo "Restarting Jenkins (docker compose with user ID ${CURRENT_UID}) ..."
